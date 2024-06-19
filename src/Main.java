c9be683faedd3e6366f476dc335451457dd3b0e6
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-06-19 19:25:02 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ import java.util.stream.LongStream;
 public class Main {
     public static void main(String[] args) {
 
-        getArithmeticProgressionSum();
+        getArithmeticProgressionSum(10000000L, 1000000000L);
 
     }
 
-    public static void getArithmeticProgressionSum() {
+    public static void getArithmeticProgressionSum(long a, long b) {
 
-        LongStream longStream = LongStream.range(10000000L, 1000000000L);
+        if (a>b) {
+            long temp = b;
+            b = a;
+            a = temp;
+        }
+
+        LongStream longStream = LongStream.range(a, b);
         System.out.println(longStream.sum());
 
     }
